45c85258ec307a4af1ac981bb82ab25cc0f2deb4
azabla <andualemchanie.y@gmail.com>
2026-02-15 03:39:55 +0300

    registration and login functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
+from blog_main.forms import RegistrationForm
 from blogs.models import Blog, About, SocialLink
 
 
@@ -16,3 +19,39 @@ def home(request):
         # "social_links": social_links,
     }
     return render(request, "home.html", context)
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("register")
+    else:
+        form = RegistrationForm()
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect("home")
+
+    form = AuthenticationForm()
+
+    context = {"form": form}
+
+    return render(request, "login.html", context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("home")
