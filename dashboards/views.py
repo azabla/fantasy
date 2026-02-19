2b2b6ccf030a1dbbcab10a739333729944b2f2a8
azabla <andualemchanie.y@gmail.com>
2026-02-19 22:24:38 +0300

    catagory list and add

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from blogs.models import Blog, Category
+from django.contrib.auth.decorators import login_required
+
+from .forms import CategoryForm
 
 # Create your views here.
 
 
+@login_required(login_url="login")
 def dashboard(request):
     total_cat = Category.objects.all().count()
     total_post = Blog.objects.all().count()
@@ -13,3 +17,18 @@ def dashboard(request):
         "total_post": total_post,
     }
     return render(request, "dashboards/dashboard.html", context)
+
+
+def categories(request):
+    return render(request, "dashboards/categories.html")
+
+
+def add_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("categories")
+    form = CategoryForm()
+    context = {"form": form}
+    return render(request, "dashboards/add_category.html", context)
